c202bb653cd9521681e64a281e9064923823a4c4
tushar bansal <tuskibansal@gmail.com>
2017-10-09 04:08:37 +0530

    structure

---
+++ calculator.js
@@ -0,0 +1,78 @@
+var final = "";
+function fun(id){
+var  text;
+	
+   if(id === 'id1'){
+   	text = "1";
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+   if(id === 'id2'){
+   	text = '2';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id3'){
+   	text = '3';
+  	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id4'){
+   	text = '4';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id5'){
+   	text = '5';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id6'){
+   	text = '6';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id7'){
+   	text = '7';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id8'){
+   	text = '8';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id9'){
+   	text = '9';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'id0'){
+   	text = '0';
+   	text = final + text;
+   	final = text;
+   	document.getElementById("display").innerHTML = final;
+   }
+
+   if(id === 'idequal'){
+
+   }
+
+}
